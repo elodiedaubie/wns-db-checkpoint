0b95e1dbbb44af5641fdc844fcf98947a69946ad
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-23 16:12:54 +0200

    add query to get all candidates for wcs compagny

--- queries/3_all_candidates_for_jobs_of_one_company.sql
+++ queries/3_all_candidates_for_jobs_of_one_company.sql
@@ -1,2 +1,8 @@
 # Écris une requêtes SQL qui retourne tous les candidats ayant répondu à 
 # au moins une offre d'une entreprise identifiée par son nom
+
+SELECT DISTINCT firstname, lastname, email, phone, Candidate.description FROM Candidate 
+JOIN Candidate_offer ON Candidate_offer.candidate_id = Candidate.id 
+JOIN Offer ON Offer.id = Candidate_offer.offer_id
+JOIN Compagny ON Compagny.id = Offer.compagny_id
+WHERE Compagny.name = 'wcs';
